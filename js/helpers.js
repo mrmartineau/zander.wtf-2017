ea4fc1147724400c16add6d6c0e902a776631fcf
Zander Martineau <zander@martineau.tv>
2013-11-22 00:22:28 +0000

    add CSS styler helper

--- js/helpers.js
+++ js/helpers.js
@@ -11,3 +11,39 @@ if ( !Array.prototype.remove ) {
 		return this.push.apply(this, rest);
 	};
 }
+
+// Get/Set CSS styles with ease
+function styler(el) {
+	return {
+		/* Get CSS style
+		 * @prop : String - CSS property name e.g 'width', 'height'
+		 * @int  : Boolean
+		 */
+		get : function(prop, int) {
+			/* TODO:
+			 * Get multiple CSS properties. prop could be comma separated list
+			 * Allow get all css values
+			 * Throw errors when args not in correct format
+			 */
+			var int = int || false;
+
+			if (int === true) {
+				return parseInt(window.getComputedStyle(el, null).getPropertyValue(prop), 10);
+			} else{
+				return window.getComputedStyle(el, null).getPropertyValue(prop);
+			}
+		},
+
+		/* Set CSS style
+		 * @prop : String - CSS property name e.g 'width', 'height'
+		 * @val  : String - CSS property value e.g. '200px'
+		 */
+		set : function(prop, val) {
+			/* TODO:
+			 * Set multiple CSS properties. prop could be comma separated list
+			 * Throw errors when args not in correct format
+			 */
+			return el.style[prop] = val;
+		}
+	};
+};
