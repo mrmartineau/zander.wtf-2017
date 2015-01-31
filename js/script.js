6befb685f8ce4f02af325e35256e8784e3b1c8dc
Zander Martineau <zander@martineau.tv>
2015-01-31 11:40:44 +0000

    lazy load comments

--- js/script.js
+++ js/script.js
@@ -7,6 +7,8 @@
 
 		var swiftclick = SwiftClick.attach(document.body);
 		var navBtn     = document.querySelector('.navBtn');
+		var commentsToggleBtn = document.querySelector('.loadComments');
+		var disqusEl          = document.querySelector('#disqus_thread');
 
 		navBtn.addEventListener('click', function(e) {
 			e.preventDefault();
@@ -19,6 +21,33 @@
 		});
 
 		scrollConverter.activate();
+
+
+		commentsToggleBtn.addEventListener('click', toggleComments);
+
+		function toggleComments(e) {
+			e.preventDefault();
+			commentsToggleBtn.classList.toggle('is-hidden');
+			disqusEl.classList.toggle('is-hidden');
+			loadJS('//' + disqus_shortname + '.disqus.com/embed.js', viewComments);
+		}
+
+		function viewComments() {
+			console.log("scroll me down", window.scrollY);
+		}
+
+		function loadJS( src, cb ){
+			"use strict";
+			var ref = window.document.getElementsByTagName( "script" )[ 0 ];
+			var script = window.document.createElement( "script" );
+			script.src = src;
+			script.async = true;
+			ref.parentNode.insertBefore( script, ref );
+			if (cb && typeof(cb) === "function") {
+				script.onload = cb;
+			}
+			return script;
+		}
 
 	});
 
