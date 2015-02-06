558741c57a4edb5fca57b94fe164495a920ab69b
Zander Martineau <zander@martineau.tv>
2015-02-06 00:12:46 +0000

    remove redundant code

--- js/modules/comments.js
+++ js/modules/comments.js
@@ -2,13 +2,9 @@ var loadJS = require('./loadJS.js');
 
 function Comments() {
 	var commentsToggleBtn = document.querySelector('.loadComments');
-	console.log("Comments!!");
 
 	if (commentsToggleBtn === null) {
-		console.log("comments not there");
 		return;
-	} else {
-		console.log("comments there");
 	}
 
 	var disqusEl          = document.querySelector('#disqus_thread');
