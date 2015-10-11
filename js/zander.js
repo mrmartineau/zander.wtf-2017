bf4f6be95adfc7a661e6a1a90f4f9b70edc746bc
Zander Martineau <zander@martineau.tv>
2015-10-11 13:05:11 +0100

    tidy file

--- js/zander.js
+++ js/zander.js
@@ -6,17 +6,12 @@ var css = require('../scss/kickoff');
 
 var SwiftClick     = require('swiftclick');
 var trak           = require('trak.js');
-
 var toggleComments = require('./modules/comments.js');
 var navigation     = require('./modules/navigation.js');
 
 document.addEventListener('DOMContentLoaded', function() {
 	trak.start();
 	var swiftclick = SwiftClick.attach(document.body);
-
-	// scrollConverter.activate();
-
 	navigation();
 	toggleComments();
-
 });
