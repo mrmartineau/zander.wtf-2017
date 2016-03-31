4e00c164ace3b846fae65c20d62dabfead452e74
Zander Martineau <zander@martineau.tv>
2016-03-31 01:01:15 -0500

    :+1: bring in the latest from Kickoff and merge old styles into new

---
+++ assets/src/js/modules/moduleTest.js
@@ -0,0 +1,16 @@
+/*
+	moduleTest.js
+	Example module to show how to include other JS files into you browserify build
+*/
+
+// dependencies for this module go here
+
+function init() {
+	doSomething();
+}
+
+function doSomething() {
+	console.log('Something was done :)');
+}
+
+module.exports = init;
---
+++ assets/src/js/script.js
@@ -0,0 +1,22 @@
+/**
+ * Project Name:
+ * Client:
+ * Author:
+ * Company:
+ */
+
+// npm modules
+import ready from 'lite-ready';
+
+// Bundle global libs that don't return a value
+import 'console';
+
+// Add your project-specific modules here
+// import moduleTest from './modules/moduleTest';  // this is a test, uncomment this line to try it
+
+
+// Your DOM ready code goes in here
+ready(() => {
+	// moduleTest(); // this is a test, uncomment this line to try it
+
+});
