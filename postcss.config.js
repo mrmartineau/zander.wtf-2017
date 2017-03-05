bd48f52bbf2d49105006dfc9005cdded0bcee2a2
Zander Martineau <zander@martineau.tv>
2017-03-05 23:31:27 +0000

    prune

--- postcss.config.js
+++ postcss.config.js
@@ -5,14 +5,13 @@ module.exports = {
 		require('postcss-mixins'), // https://github.com/postcss/postcss-mixins
 		require('postcss-advanced-variables'), // https://github.com/jonathantneal/postcss-advanced-variables
 		require('postcss-custom-properties'), // https://github.com/postcss/postcss-custom-properties
-		require('pixrem'), // https://github.com/robwierzbowski/node-pixrem
-		//require('lost'), // http://lostgrid.org/docs.html
+		// require('lost'), // http://lostgrid.org/docs.html
 		// require('postcss-utilities'), // https://github.com/ismamz/postcss-utilities
 		require('postcss-compact-mq'), // https://github.com/rominmx/postcss-compact-mq
 		require('postcss-modular-scale'), // https://github.com/kristoferjoseph/postcss-modular-scale
 		require('postcss-nested'), // https://github.com/postcss/postcss-nested
 		require('postcss-nested-ancestors'), // https://github.com/toomuchdesign/postcss-nested-ancestors
-		require('postcss-reporter')({ clearMessages: true }), // https://github.com/postcss/postcss-reporter
+		// require('postcss-reporter')({clearReportedMessages: true}), // https://github.com/postcss/postcss-reporter
 		require('autoprefixer')({ browsers: ['> 1% in US', 'last 2 versions', 'iOS > 8'] }), // https://github.com/postcss/autoprefixer
 	]
 }
